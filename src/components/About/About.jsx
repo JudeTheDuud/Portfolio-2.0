794f85d1882e1318ba4e9c3663b25d8d08a3c2dd
JudeTheDuud <praiseosai@gmail.com>
2025-01-06 21:42:12 +0100

    Styles Updates

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -14,14 +14,14 @@ export const About = () => {
             <div className={styles.aboutItemText}>
               <h3>Frontend Developer</h3>
               <p>
-                Hi, I`m Juda, a passionate Front-End Engineer dedicated
+                As a passionate Front-End Engineer dedicated
                 to creating beautiful, responsive, and user-friendly web
-                applications. <br /> I specialize in building interactive interfaces
+                applications, I specialize in building interactive interfaces
                 with modern technologies like React.js, ensuring performance
                 optimization, and delivering seamless user experiences across
-                all devices. <br /> My expertise includes API integration,
+                all devices.<br /> My expertise includes API integration,
                 accessibility compliance, and collaborating with
-                cross-functional teams to bring ideas to life. <br /> I thrive on
+                cross-functional teams to bring ideas to life.<br /> I thrive on
                 solving challenges, making the web accessible, and crafting
                 digital experiences that leave a lasting impression.
               </p>
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -9,7 +9,7 @@ export const Hero = () => {
       <div className={styles.content}>
         <h1 className={styles.title}>Hi, I'm Juda</h1>
         <p className={styles.description}>
-          I'm a front-end developer with 3 years of experience using React and
+          I'm a Front-end developer with 3 years of experience using React and
           NodeJS. Reach out if you'd like to learn more!
         </p>
         <a href="mailto:judaosai@gmail.com" className={styles.contactBtn}>
